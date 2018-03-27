6a78dddd0fcdaf5b5d4d3a4ba37613a757e1a7a1
pkallas <pkallas29@gmail.com>
2018-03-27 11:14:37 -0700

    Renamed withUrl to withFetch

--- src/client/components/SearchResult/index.js
+++ src/client/components/SearchResult/index.js
@@ -1,14 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import LocalAPIButton from '../LocalAPIButton';
 import GithubSearch from '../GithubSearch';
-import { withUrl } from '../withUrl';
+import { withFetch } from '../withFetch';
 
-const LocalAPIButtonWithUrl = withUrl(LocalAPIButton, '/api/localAPIButton');
+const LocalAPIButtonWithUrl = withFetch(LocalAPIButton, '/api/localAPIButton');
 
 export default class SearchResult extends Component {
   constructor(props) {
     super(props);
-    this.GithubSearchWithUrl = withUrl(GithubSearch,
+    this.GithubSearchWithUrl = withFetch(GithubSearch,
     `https://api.github.com/users/${this.props.match.url.replace(/\/searchresult\//, '')}`);
   }
 
