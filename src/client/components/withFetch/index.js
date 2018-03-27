2e6b911ce2796dc4115e30da8dc04e755d0d6908
pkallas <pkallas29@gmail.com>
2018-03-27 11:26:38 -0700

    Fixed typo in withFetch directory

--- src/client/components/withFetch/index.js
+++ src/client/components/withFetch/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-export function withUrl(WrappedComponent, url) {
+export function withFetch(WrappedComponent, url) {
   return class extends Component {
     constructor(props) {
       super(props);
